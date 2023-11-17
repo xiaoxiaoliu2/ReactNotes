63325169172f225a9fcffc81c59ee1a35a068803
xiaoxiaoliu2 <l.margaret.liu@gmail.com>
2023-11-17 23:54:40 +0800

    React Essencial: useState, render conditionally

--- React-Essentials/src/App.jsx
+++ React-Essentials/src/App.jsx
@@ -1,5 +1,7 @@
 // import componentsImg from './assets/components.png';
+import { useState } from 'react';     // only call Hooks inside of component functions, and on the top level
 import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
@@ -11,11 +13,16 @@ import TabButton from './components/TabButton.jsx';
 // to output the content of the header component as part of our app, use it in App component
 
 //  React components are just JS functions that returns JSX code
+// react will only execute a component function once, when the button clicked, the whole app will not be eexecuted again, onely the console log message from inside the event handler function, so the normal variable will not work to update the UI, so we need to work with state
 function App() {
-  function handleSelect() {
-    console.log(`Hello World!`);
+  const [ selectedTopic, setSelectedTopic ] = useState();      // always return 2 elements in an array, 1. initial state value: current data snapshot for this component execution cycle 2. updated value, a function provided by react that can be executed to update this state(stored value), telling react that this component function must be executed again
+
+  function handleSelect(selectedButton) {
+    // selectedButton => `components`, `jsx`, `props`, `state`
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
-  
+
   return (
     <div>
       <Header />
@@ -52,11 +59,24 @@ function App() {
           <h2>Examples</h2>
           {/* menu is HTML element, and is used to create a list of buttons */}
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton isSelected={selectedTopic ===`components`} onSelect={() => handleSelect(`components`)}>Components</TabButton>
+            <TabButton isSelected={selectedTopic ===`jsx`} onSelect={() => handleSelect(`jsx`)}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic ===`props`} onSelect={() => handleSelect(`props`)}>Props</TabButton>
+            <TabButton isSelected={selectedTopic ===`state`} onSelect={() => handleSelect(`state`)}>State</TabButton>
           </menu>
+          {!selectedTopic ? (
+          <p>Please select a topic.</p> 
+          ): (
+          <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+        )}
+
+          
         </section>
       </main>
     </div>
--- React-Essentials/src/components/TabButton.jsx
+++ React-Essentials/src/components/TabButton.jsx
@@ -1,10 +1,12 @@
-export default function TabButton({ children, onSelect }) {   // children prop contains content  between component text in App
+export default function TabButton({ children, onSelect, isSelected }) {   // children prop contains content  between component text in App
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? `active` : undefined} onClick={onSelect}>{children}</button>  
     </li>
   );
 }
+// {}: if we want dynamic or JS-based value
+// Add a class name to a button: in JSX, use className instead of what we use in orignal HTML class
 // pattern 2: handleSelect>onSelect>TabButton
 // handleClick used as a value passed to the click prop
 
