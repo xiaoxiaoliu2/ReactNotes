257ff9138c633c6ef4111b491a654bbb65d0971c
xiaoxiaoliu2 <l.margaret.liu@gmail.com>
2023-11-22 21:57:49 +0800

    tic-toc-toe handle Edit button

--- Section4-tic-tac-toe-project/src/Player.jsx
+++ Section4-tic-tac-toe-project/src/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // setIsEditing(isEditing ? false : true);
+    setIsEditing((editing) => !editing); // use a function to update the state
+    // setIsEditing(!isEditing);   // directly invert the value
+  }
+
+  let playerName = <span className="player-name">{name}</span>
+  let btnCaption = "Edit";
+  if (isEditing) {
+    playerName = < input type="text" required value={name}/>;
+    btnCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 
