ac4479576eae8bd76dfbee62462c12a6f9cba9ec
xiaoxiaoliu2 <l.margaret.liu@gmail.com>
2023-11-26 13:20:27 +0800

    Section10: ContextAPI

---
+++ Section6-Styling-Tailwind/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.png';
+
+export default function Header() {
+  return (
+    <header className='flex flex-col items-center mt-8 mb-16'>
+      <img src={logo} alt="A canvas" className='mb-8 w-44 h-44 object-contain' />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </header>
+  );
+}
