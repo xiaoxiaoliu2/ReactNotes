1271247675a772d59b3dce2d849722a0e6fc38de
xiaoxiaoliu2 <l.margaret.liu@gmail.com>
2023-11-26 22:57:22 +0800

    Section10: outsource Context & State

--- Section10-ContextAPI-useReducer/src/components/Header.jsx
+++ Section10-ContextAPI-useReducer/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { CartContext } from '../store/shopping-cart-context.jsx';
 export default function Header({ cart, onUpdateCartItemQuantity }) {
   const modal = useRef();
   const { items } = useContext(CartContext);
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
